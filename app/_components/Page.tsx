a0a7cf89e1af8a1644bdfcfdd7c5da23467bb9a7
Nacho Betancourt <trsgarp@gmail.com>
2024-05-10 13:01:35 -0600

    suspense page

---
+++ app/_components/Page.tsx
@@ -0,0 +1,52 @@
+import About from './About';
+import Contact from './Contact';
+import Header from './Header';
+import ProjectModal from './ProjectModal';
+import Projects, { Project } from './Projects';
+
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { cookieLanguage, setCookieLanguage } from '../_actions/language';
+
+const Page = () => {
+  const [language, setLanguage] = useState<string>('es');
+  const [project, setProject] = useState<Project | null>(null);
+  const searchParams = useSearchParams();
+
+  const changeLanguage = (lang: string) => {
+    setLanguage(lang || 'es');
+    setCookieLanguage(lang || 'es');
+  };
+
+  useEffect(() => {
+    const setLng = async () => {
+      const lang = await cookieLanguage();
+      setLanguage(lang?.value || 'es');
+    };
+    const urlLang =
+      searchParams.get('lang') || searchParams.get('language') || '';
+    if (urlLang && ['en', 'es'].includes(urlLang)) {
+      changeLanguage(urlLang);
+    } else {
+      setLng();
+    }
+  }, [searchParams]);
+
+  return (
+    <>
+      <Header lang={language} changeLanguage={changeLanguage} />
+      <About lang={language} />
+      <Projects lang={language} setProject={setProject} />
+      <Contact lang={language} />
+      {project && (
+        <ProjectModal
+          lang={language}
+          project={project}
+          setProject={setProject}
+        />
+      )}
+    </>
+  );
+};
+
+export default Page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,14 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
-import { Suspense, useEffect, useState } from 'react';
-import { cookieLanguage, setCookieLanguage } from './_actions/language';
-import About from './_components/About';
-import Contact from './_components/Contact';
-import Header from './_components/Header';
-import ProjectModal from './_components/ProjectModal';
-import Projects, { Project } from './_components/Projects';
-import styles from './_styles/page.module.scss';
+import { Suspense } from 'react';
+import Page from './_components/Page';
+import styles from './_styles/Page.module.scss';
 
 export default function Home() {
-  const [language, setLanguage] = useState<string>('es');
-  const [project, setProject] = useState<Project | null>(null);
-  const searchParams = useSearchParams();
-
-  const changeLanguage = (lang: string) => {
-    setLanguage(lang || 'es');
-    setCookieLanguage(lang || 'es');
-  };
-
-  useEffect(() => {
-    const setLng = async () => {
-      const lang = await cookieLanguage();
-      setLanguage(lang?.value || 'es');
-    };
-    const urlLang =
-      searchParams.get('lang') || searchParams.get('language') || '';
-    if (urlLang && ['en', 'es'].includes(urlLang)) {
-      changeLanguage(urlLang);
-    } else {
-      setLng();
-    }
-  }, [searchParams]);
-
   return (
     <main className={styles.main}>
       <Suspense fallback={<p>...</p>}>
-        <Header lang={language} changeLanguage={changeLanguage} />
-        <About lang={language} />
-        <Projects lang={language} setProject={setProject} />
-        <Contact lang={language} />
-        {project && (
-          <ProjectModal
-            lang={language}
-            project={project}
-            setProject={setProject}
-          />
-        )}
+        <Page />
       </Suspense>
     </main>
   );
