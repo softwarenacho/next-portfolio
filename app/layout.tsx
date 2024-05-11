b3a67c2b9ffaa71cb85e484a5a294d7d45f6c787
Nacho Betancourt <trsgarp@gmail.com>
2024-05-11 01:31:21 -0600

    icon!

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <link rel='icon' href='/favicon.ico' sizes='any' />
+      </head>
       <body>{children}</body>
     </html>
   );
